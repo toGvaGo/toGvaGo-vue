8564ce618cdc4b69a8ab9104a2327af3e4580960
xiewentao <1063109979@qq.com>
2024-11-11 21:20:47 +0800

    Element主流程

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -9,9 +9,41 @@ function patch(vnode, container) {
 
     //TODO
     //判断是 element 类型还是 component 类型
-    //processElement();
-    processComponent(vnode, container);
+    console.log(vnode.type);
+    if (typeof vnode.type === 'string') {
+        processElement(vnode, container);
+    } else if (typeof vnode.type === 'object') {
+        processComponent(vnode, container);
+    }
 }
+
+function processElement(vnode, container) {
+    mountElement(vnode, container);
+}
+function mountElement(vnode, container) {
+    let el = document.createElement(vnode.type);
+
+    const { props } = vnode;
+    for (const key in props) {
+        const val = props[key];
+        el.setAttribute(key, val);
+    }
+    const children = vnode.children;
+    if (typeof children === 'string') {
+        el.textContent = children;
+    } else if (Array.isArray(children)) {
+        mountChildren(children, el);
+    }
+
+    container.appendChild(el);
+}
+
+function mountChildren(vnode, container) {
+    vnode.forEach(v => {
+        patch(v, container);
+    })
+}
+
 
 function processComponent(vnode: any, container: any) {
     mountComponent(vnode, container);
@@ -30,3 +62,4 @@ function setupRenderEffect(instance: any, container: any) {
     patch(subTree, container);
 
 }
+
