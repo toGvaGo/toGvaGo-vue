60c2118560b6564be72487126c45b637d19ad9d6
xiewentao <1063109979@qq.com>
2024-12-15 17:38:15 +0800

    feat: 三种类型联合解析

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -9,35 +9,62 @@ const enum TagType {
 export function baseParse(content: string) {
     const context = createParserContext(content);
 
-    return createRoot(parseChildren(context))
+    return createRoot(parseChildren(context, []))
 }
 
-function parseChildren(context) {
+function parseChildren(context, ancestors) {
     const nodes: any = [];
 
-    let node
-    const s = context.source;
-    if (s.startsWith('{{')) {
-        node = parseInterpolation(context);
-    } else if (s[0] === '<') {
-        if (/[a-z]/i.test(s[1])) {
-            node = parseElement(context);
+    while (!isEnd(context, ancestors)) {
+        let node
+        const s = context.source;
+        if (s.startsWith('{{')) {
+            node = parseInterpolation(context);
+        } else if (s[0] === '<') {
+            if (/[a-z]/i.test(s[1])) {
+                node = parseElement(context, ancestors);
+            }
         }
+
+        if (!node) {
+            node = parseText(context)
+        }
+
+        nodes.push(node)
     }
-
-    if (!node) {
-        node = parseText(context)
-    }
-
-    nodes.push(node)
-
-
 
     return nodes
 }
 
+function isEnd(context: any, ancestors) {
+    const s = context.source
+    if (s.startsWith('</')) {
+        for (let i = ancestors.length - 1; i >= 0; i--) {
+            const tag = ancestors[i].tag;
+            if (startsWithEndTagOpen(s, tag)) {
+                return true
+            }
+        }
+    }
+
+    // if (parentTag && s.startsWith(`</${parentTag}>`)) {
+    //     return true;
+    // }
+    return !s
+}
+
 function parseText(context: any) {
-    const content = parseTextData(context, context.source.length);
+    let endIndex = context.source.length
+    let endTokens = ["<", "{{"];
+
+    for (let i = 0; i < endTokens.length; i++) {
+        const index = context.source.indexOf(endTokens[i]);
+        if (index !== -1 && index < endIndex) {
+            endIndex = index;
+        }
+    }
+
+    const content = parseTextData(context, endIndex);
 
     return {
         type: NodeTypes.TEXT,
@@ -52,14 +79,28 @@ function parseTextData(context: any, length) {
     return content;
 }
 
-function parseElement(context) {
+function parseElement(context, ancestors) {
+    const element: any = parseTag(context, TagType.START);
 
+    ancestors.push(element);
+    element.children = parseChildren(context, ancestors);
+    ancestors.pop();
 
-    const element = parseTag(context, TagType.START);
-    parseTag(context, TagType.END);
+    if (startsWithEndTagOpen(context.source, element.tag)) {
+        parseTag(context, TagType.END);
+    } else {
+        throw new Error(`Tag "${element.tag}" must be closed`);
+    }
+
 
     return element
 }
+
+function startsWithEndTagOpen(source, tag) {
+    return source.startsWith('</') && source.slice(2, 2 + tag.length).toLowerCase() === tag.toLowerCase();
+}
+
+
 function parseTag(context: any, type: TagType) {
 
     const match: any = /^<\/?([a-z]*)/i.exec(context.source);
