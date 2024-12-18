d373b766e53649b63e3eeaa80622aff682923ddd
xiewentao <1063109979@qq.com>
2024-12-18 22:10:09 +0800

    feat: 将template转化为render函数、根据vue架构图调整包依赖关系

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -310,7 +310,7 @@ export function createRenderer(options) {
             if (!instance.isMounted) {
                 console.log('init')
                 const { proxy } = instance
-                const subTree = (instance.subTree = instance.render.call(proxy))
+                const subTree = (instance.subTree = instance.render.call(proxy, proxy))
                 console.log(subTree);
 
                 patch(null, subTree, container, instance, anchor);
@@ -327,7 +327,7 @@ export function createRenderer(options) {
                 }
 
                 const { proxy } = instance
-                const subTree = instance.render.call(proxy);
+                const subTree = instance.render.call(proxy, proxy);
                 const prevSubTree = instance.subTree;
                 instance.subTree = subTree;
 
---
+++ src/shared/toDisplayString.ts
@@ -0,0 +1,6 @@
+
+
+
+export function toDisplayString(value) {
+    return String(value)
+}
